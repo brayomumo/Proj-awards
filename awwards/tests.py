191383f82dc21fa690ed202732590a3615174a1a
Brian <brayomumo5@gmail.com>
2019-09-05 17:22:58 +0300

    first test

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Project
 
 # Create your tests here.
+class test_projects(TestCase):
+    def setUp(self):
+        self.proj1 = Project(title="The grand proj",description="the grand coded project")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.proj1, Project))
+
+    def test_save_method(self):
+        self.proj1.save_project()
+        proj = Project.objects.all()
+        self.assertTrue(len(proj) > 0 )
