519303fed51adec3890c8549b267decfa14da94a
Brian <brayomumo5@gmail.com>
2019-09-05 17:22:20 +0300

    Create database

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    # image = models.ImageField(upload_to='posts/', default='')
+    description = models.CharField(max_length=150)
+    date_posted = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
