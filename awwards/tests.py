1989ea8ad8fbe1f5dcab8dafe12d1f963d9362c9
Brian <brayomumo5@gmail.com>
2019-09-06 08:14:49 +0300

    Final tests

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,19 +1,39 @@
+
 from django.test import TestCase
-from .models import Project
+from django.contrib.auth.models import User
+from .models import Project, Profile, Review
 
 # Create your tests here.
-class test_projects(TestCase):
+class ProjectTestClass(TestCase):
+    # Set up method
     def setUp(self):
-        self.proj1 = Project(title="The grand proj",description="the grand coded project")
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
 
+    #Testing instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.proj1, Project))
+        self.assertTrue(isinstance(self.project,Project))
 
-    def test_save_method(self):
-        self.proj1.save_project()
-        proj = Project.objects.all()
-        self.assertTrue(len(proj) > 0 )
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
 
-    def test_delete(self, pk):
-        # self.proj1.delete_project()
-        self.assertTrue((self.proj1.delete_projects()) == (self.proj1.delete()))
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+class ReviewTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
+        self.profile.save()
+        self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
+        self.project.save()
+        self.review = Review(id=1,project=self.project,design=10,usability=10,content=10,average=7,user=self.user)
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.review,Review))
