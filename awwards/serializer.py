e6e24dc964278292cfa9936bb87a4ecda7d0ff03
Brian <brayomumo5@gmail.com>
2019-09-06 17:13:44 +0300

    Create serializer models

--- awwards/serializer.py
+++ awwards/serializer.py
@@ -1,1 +1,13 @@
-from 
+from rest_framework import serializers
+from .models import Profile, Project
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('profile_picture','prof_user', 'bio', 'all_projects')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title', 'details', 'link', 'design', 'usability', 'content')
