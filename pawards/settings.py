f2d76920c8f941bfd0bf1136a6b903a409dea914
Brian <brayomumo5@gmail.com>
2019-09-06 16:42:39 +0300

    Static file error

--- pawards/settings.py
+++ pawards/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'social_django',
+    'tinymce',
     'django.contrib.admin',
     'awwards',
     'bootstrap3',
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'pawards.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awards',
+        'NAME': 'pawards',
         'USER': 'smoucha',
         'PASSWORD': 'mumo'
     }
@@ -146,3 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
