ca98a6908b8f8a0585e852b8253f6339804ae62f
Brian <brayomumo5@gmail.com>
2019-09-06 16:41:59 +0300

    migrations

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Project, Review
+from .models import Profile, Project
 
 # Register your models here.
 admin.site.register(Project)
-admin.site.register(Review)
