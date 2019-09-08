452b227995d695526454c22e93deb3fbc5efb65a
Brian <brayomumo5@gmail.com>
2019-09-08 23:44:35 +0300

    Logout

--- pawards/urls.py
+++ pawards/urls.py
@@ -23,8 +23,9 @@ urlpatterns = [
     url('admin/', admin.site.urls),
     # url(r'^login/$', auth_views.auth_login, name='login'),
     url(r'^api-token-auth',obtain_auth_token),
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.auth_logout, name='logout'),
+   url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', auth_views.logout, {"next_page": '/'}),
+
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'',include(urls), name='home' )
